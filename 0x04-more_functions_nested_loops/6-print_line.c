2baf83872a224006a3bb9173386af14399d27e3f
Orlando Alverio <orlando.alverio1@gmail.com>
2022-02-16 00:25:03 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_line - print n times ____
+*@n: integrer
+*Return: 0
+*/
+void print_line(int n)
+{
+	int a;
+
+	if (n != 0)
+	{
+	for (a = 0; a < n; a++)
+		{
+		_putchar('_');
+		}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+
