ad19ba6692340d2c34480109695354f1f961d6a0
Orlando Alverio <orlando.alverio1@gmail.com>
2022-02-16 00:26:09 +0000

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_diagonal -print diganola lines
+*@n: integrer
+*Return: 0
+*/
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
