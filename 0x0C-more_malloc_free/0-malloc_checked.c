87fcf9c784cca741cc0e644ca92a9c8707724c65
Orlando Alverio <orlando.alverio1@gmail.com>
2022-03-09 01:06:13 +0000

    checking malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*malloc_checked -functoin that alocates memory
+*@b: int
+*Return: 0
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
