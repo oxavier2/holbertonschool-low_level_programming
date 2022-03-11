e21480085ab753a57c8444527be45b1b91db58e6
Orlando Alverio <orlando.alverio1@gmail.com>
2022-03-11 02:03:40 +0000

    varfunc

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIDIC_FUNCTIONS
+#define VARIDIC_FUNCTIONS
+#include <stdarg.h>
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+#endif
